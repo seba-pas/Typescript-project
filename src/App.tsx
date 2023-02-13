a15af3831759ffd42943fcdf652f3090e04d4698
Sebastian Pastorenzi <102265528+seba-pas@users.noreply.github.com>
2023-02-13 06:33:12 -0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
+import List from './components/List';
 import './App.css';
 
+
+
+
+ 
+
+interface Sub {
+  nick: string
+  avatar: string
+  subMonths: number
+  description?: string
+
+}
+
+interface AppState {
+  subs: Array<Sub>
+  newSubsNumber: number
+}
+
+const INITIAL_STATE = [
+
+  {
+  nick: 'dapelu',
+  subMonths: 3,
+  avatar: 'https://i.pravatar.cc/150?u=dapelu',
+  description: 'dapelu hace de moderador'
+  },
+  {
+  nick: 'seba_pas',
+  subMonths: 9,
+  avatar: 'https://i.pravatar.cc/150?u=sebapas'
+  }
+
+]
+    
+
+
 function App() {
+  const [subs, setSubs] = useState<Array<Sub>>([]) //<Sub>[]
+  const [newsSubsNumber, setNewSubsNumber] = useState<AppState['newSubsNumber']>(0)
+
+ useEffect(() => {
+setSubs(INITIAL_STATE)
+ }, [])
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>los aubs de la pagina</h1>
+<List subs={subs}/>
+     
     </div>
   );
 }
